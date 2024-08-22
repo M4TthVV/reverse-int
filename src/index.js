38772a31c295ec0925822f1fba2d3e9789617a96
M4TthVV <matveysdr@gmail.com>
2024-08-22 15:16:02 +0300

    feat: add func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    const num = n.toString()
+    let result = ''
+
+    for (let i = 1; i <= num.length; i++) {
+        result += num[num.length - i]
+    }
+
+    return result.replace('-', '')
 }
